dcb1003939d1448b80101ea1f3817f8d3fbd5065
Yash Mishra <mishrayash392@gmail.com>
2025-05-11 18:45:36 +0530

    initialized backend

---
+++ backend/src/index.ts
@@ -0,0 +1,18 @@
+console.log("yash is devootee of radha rani")
+import express from "express" 
+
+const app = express()
+app.use(express.json()) 
+
+app.post('/signup',async (req,res)=>{
+    try{
+        
+    }catch{
+
+    }
+})
+
+app.listen(3000,()=>{
+    console.log("server is running !!")
+})
+ 
